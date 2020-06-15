fff603a86c0c682fd15fb472e1a10a0bad2d642a
Alex <vandropten7@gmail.com>
2020-06-15 16:36:32 +0100

    basic client server communication

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Dependencies
 const express = require("express");
-const hbs = require("express-handlebars")
+const hbs = require("express-handlebars");
 const app = express();
 
 //Env variables
@@ -10,15 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 
 //Setting up handlebars
-
 app.engine("hbs", hbs({ extname: "hbs", defaultLayout: "layout", layoutsDir: "./views/layouts", }));
 app.set('view engine', 'hbs');
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //Allows us to reference anything from the public folder
 //Use this command to add other folders with content
-app.use(express.static('public'))
-
-
+app.use(express.static('public'));
 
 //Setting up routes
 //Use this command to add other files with routes
